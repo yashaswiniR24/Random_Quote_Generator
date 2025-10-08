c61e56094be13e97516e1942714ef618198f105e
yashaswiniR24 <yashaswinirao.bhogarajula@gmail.com>
2025-10-08 16:07:16 +0530

    added ppt and git pull

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ async function getQuote() {
   authorText.textContent = "";
 
   try {
-    const response = await fetch("https://api.quotable.io/random");
+    const response = await fetch("http://api.quotable.io/random");
     const data = await response.json();
 
     quoteText.textContent = `"${data.content}"`;
